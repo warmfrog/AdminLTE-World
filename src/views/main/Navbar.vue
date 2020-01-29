af05ede8c713b3eb35d23358851774fafe502e6e
libo <1594914459@qq.com>
2020-01-29 16:01:09 +0800

    add konwledge

--- src/views/main/Navbar.vue
+++ src/views/main/Navbar.vue
@@ -22,9 +22,9 @@
         </ul>
 
         <!-- SEARCH FORM -->
-        <form class="form-inline ml-0 ml-md-3">
+        <form class="form-inline ml-0 ml-md-2">
           <div class="input-group input-group-sm">
-            <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+            <input class="form-control form-control-navbar" type="search" placeholder="搜索" aria-label="Search">
             <div class="input-group-append">
               <button class="btn btn-navbar" type="submit">
                 <i class="fas fa-search"></i>

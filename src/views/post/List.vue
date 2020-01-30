da52983c533d963dff857f8e5d3fc900c8e1fb9a
libo <1594914459@qq.com>
2020-01-30 08:33:34 +0800

    PostItem

--- src/views/post/List.vue
+++ src/views/post/List.vue
@@ -1,13 +1,69 @@
 <template>
-  <div>
+   <div class="content-wrapper" style="min-height: 1416.81px;">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Posts</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Post</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
 
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="card card-solid">
+        <div class="card-body pb-0">
+          <div class="row d-flex align-items-stretch">
+            <PostItem v-for="post in posts" :key="post.uuid" :post="post"></PostItem>
+          </div>
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+          <nav aria-label="Contacts Page Navigation">
+            <ul class="pagination justify-content-center m-0">
+              <li class="page-item active"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item"><a class="page-link" href="#">4</a></li>
+              <li class="page-item"><a class="page-link" href="#">5</a></li>
+              <li class="page-item"><a class="page-link" href="#">6</a></li>
+              <li class="page-item"><a class="page-link" href="#">7</a></li>
+              <li class="page-item"><a class="page-link" href="#">8</a></li>
+            </ul>
+          </nav>
+        </div>
+        <!-- /.card-footer -->
+      </div>
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
   </div>
 </template>
 
 <script>
+import PostItem from './PostItem'
+import postsData from './posts.json'
   export default {
-    
-    
+    name: 'PostList',
+    components: {
+      PostItem
+    },
+    data(){
+      return {
+        posts: postsData.data
+      }
+    }
   }
 </script>
 
---
+++ src/views/post/PostItem.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="col-12 col-sm-12 col-xm-12 col-md-6 d-flex align-items-stretch">
+    <div class="card bg-light text-left">
+      <div class="card-header border-bottom-0">
+        <b> {{ post.title }} </b>
+      </div>
+
+      <!-- Card Body -->
+      <div class="card-body pt-0">
+        <div class="row">
+          <div class="col-3">
+            <img src="../../assets/img/user1-128x128.jpg" alt="" class=" img-fluid mb-3">
+            <p class="text-xs">
+              <span>
+                <span class="text-red">关键字: </span>
+                <span class="text-green">{{ post.keyWords.join() }}</span>
+              </span>
+            </p>
+
+          </div>
+          <div class="col-9">
+            <span> {{ post.brief }} </span>
+          </div>
+        </div>
+      </div>
+
+      <!-- Card Footer -->
+      <div class="card-footer">
+        <div class="text-left">
+          <a href="#" class="btn btn-sm bg-outline-teal">
+            <i class="fas fa-user"></i>作者 {{ post.author.name }}
+          </a>
+          <a href="#" class="btn btn-sm bg-outline-teal">
+            <i class="fas fa-clock"></i>发布时间 {{ new Date(post.releaseTime).toLocaleString('chinese',{hour12:false}) }}
+          </a>
+          <a href="#" class="btn btn-sm bg-outline-teal">
+            <i class="fas fa-star"></i>收藏 {{ post.collectionCount }}
+          </a>
+          <a href="#" class="btn btn-sm bg-outline-teal">
+            <i class="fas fa-thumbs-up"></i> {{ post.likes }}
+          </a>
+          <a href="#" class="btn btn-sm bg-outline-teal">
+            <i class="fas fa-thumbs-down"></i> {{ post.dislikes }}
+          </a>
+          <a :href="post.downloadUrl" class="btn btn-sm bg-outline-teal">
+            <i class="fas fa-download"></i> 下载
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostItem',
+  props: {
+    'post': {
+      'uuid': String,
+      'title': String,
+      'keyWords': Array,
+      'author': {
+        'name': String,
+        'email': String,
+        'institution': String
+      },
+      'authors': Array,
+      'downloadUrl': String,
+      'releaseTime': Date,
+      'brief': String,
+      'coverImage': String,
+      'images': Array,
+      'filePath': String,
+      'content': String,
+      'wordCount': Number,
+      'readerCount': Number,
+      'likes': Number,
+      'dislikes': Number,
+      'collectionCount': Number,
+      'downloadCount': Number,
+      'commentCount': Number,
+      'releasedPlatforms': Array,
+      'referencedCount': Number,
+      'score': Number,
+      'language': String,
+      'tags': Array,
+      'classifications': Array,
+      'type': Number,
+      'references': Array
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

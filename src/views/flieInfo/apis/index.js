a7e80de4fd132cf1d6c7c18cfa8b8e760ee40c71
libo <libo0@foxmail.cn>
2021-05-09 18:10:33 +0800

    dev

--- src/views/flieInfo/apis/index.js
+++ src/views/flieInfo/apis/index.js
@@ -5,3 +5,7 @@ let listUrl = '/thunderstorm/file-info/list'
 export const fileInfoList = data => {
   return Axios.post(listUrl, data).then(res => res.data)
 }
+
+export const listByPage = data => {
+   return Axios.post(listUrl, data).then(res => res.data)
+}
